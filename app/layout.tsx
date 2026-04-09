29be52b213dfe39dce0f0061dad095aed914be94
Roshan <roshankrsu@gmail.com>
2026-04-09 19:30:38 +0530

    video upload functionality with file validation and toast notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from "react-hot-toast";
 
 
 const geistSans = Geist({
@@ -30,7 +31,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}
+        <Toaster position="top-center"/>
+      </body>
     </html>
   </ClerkProvider>
   );
